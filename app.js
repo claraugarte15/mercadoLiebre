41ecfde099254e708278023f66cf978af3c5a5c3
Renata Ugarte <renataugarte@MacBook-Pro-de-Ugarte.local>
2021-07-28 18:44:49 -0300

    terminado

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
